8ae1ac4981c2a7fc210714897cc9529916ce3704
Nata Malinova <rawr@raery.ru>
2022-12-29 04:46:02 +0700

    slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import Glide from '@glidejs/glide'
+
 console.log('hello! js worsk fine!')
+
+export const initGlide = (selector, options) => {
+  document.querySelectorAll(selector)?.forEach((element) => {
+    const glide = new Glide(element, {
+      perView: element.dataset.count || 1,
+      type: 'carousel',
+      ...options,
+    })
+    glide?.mount()
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initGlide('.primary__slider', {
+    autoplay: 5000,
+  })
+})

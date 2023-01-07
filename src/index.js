8327c1060d9e48366f1fe4b6e03e3ab87d10bf9f
Nata Malinova <rawr@raery.ru>
2023-01-08 03:18:22 +0700

    partners

--- src/index.js
+++ src/index.js
@@ -18,3 +18,16 @@ document.addEventListener('DOMContentLoaded', () => {
     autoplay: 5000,
   })
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+  initGlide('.partners__carousel', {
+    perView: 4,
+    startAt: 1,
+    gap: 0,
+    // focusAt: 4 // ?
+    // peek: {
+    //     before: 0,
+    //     after: 0
+    // }
+  })
+})
